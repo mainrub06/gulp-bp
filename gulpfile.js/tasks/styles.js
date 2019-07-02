63ba3dc4c30fe293ef0dda521b7c57c7a833b5e4
unknown <mainrub06@gmail.com>
2019-07-02 16:55:27 +0300

    02.07.19 0.01j

---
+++ gulpfile.js/tasks/styles.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const settings = require('../settings');
+const { task, src, dest } = require('gulp');
+const gulpIf = require('gulp-if');
+const plumber = require('gulp-plumber');
+const rename = require('gulp-rename');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const csso = require('gulp-csso');
+const browserSync = require('browser-sync').get('Local Server');
+
+const isDev = !process.env.NODE_ENV;
+
+
+// Компиляция стилей проекта
+task('styles', () => {
+  let pluginsPostcss = [autoprefixer()];
+
+  return src(`${settings.paths.src.styles}style.scss`)
+    .pipe(gulpIf(isDev, sourcemaps.init()))
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss(pluginsPostcss))
+    .pipe(csso({ forceMediaMerge: true, comments: false }))
+    .pipe(gulpIf(isDev, sourcemaps.write()))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(dest(settings.paths.dest.styles))
+    .pipe(gulpIf(isDev, browserSync.stream()));
+});
---
+++ source/js/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const p = $('p');
+
+p.click((it) => {
+  p.text('hay!');
+});
